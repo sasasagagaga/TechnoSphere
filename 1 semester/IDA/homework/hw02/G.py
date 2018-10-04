9a5022af8ee96456186fd3e056dbc8cf629de3c8
Alexander Chernyshev <alexander.v.chernyshev@mail.ru>
2018-10-04 23:20:16 +0300

    G fixed

--- 1 semester/IDA/homework/hw02/G.py
+++ 1 semester/IDA/homework/hw02/G.py
@@ -1,20 +1,12 @@
-def check(s):
-    delta = 0
-    for c in s:
-        delta += 1 if c == '(' else -1
-        if delta < 0:
-            return False
-    return delta == 0
-    
 def brackets(n):
-    def rec(n, s):
-        if n == 0:
-            if check(s):
-                yield s
+    def rec(n, s, delta):
+        if n == 0 and delta == 0:
+            yield s
+        if n == 0 or delta < 0:
             return
-        yield from rec(n - 1, s + '(')
-        yield from rec(n - 1, s + ')')
-    yield from rec(2 * n, '')
+        yield from rec(n - 1, s + '(', delta + 1)
+        yield from rec(n - 1, s + ')', delta - 1)
+    yield from rec(2 * n, '', 0)
 
 if __name__ == '__main__':
     print('\n'.join([str(x) for x in brackets(int(input()))]))
